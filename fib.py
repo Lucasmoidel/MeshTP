3534249e479ad827f5aded3eccb66191c6af07e3
Lucas Moidel <lucas122133@gmail.com>
2025-07-24 23:33:01 -0700

    🐍😭😡⏳ i hate python

--- fib.py
+++ fib.py
@@ -1,7 +1,9 @@
+import sys
+#sys.set_int_max_str_digits(100000000)
 numbers=[0, 1]
-for i in range(2, 200):
+for i in range(2, 500):
     numbers.append(numbers[i-1]+numbers[i-2])
-print(numbers)
+#print(numbers)
 f = open("testfile.txt", "w")
 for x in numbers:
     f.write(str(x) + "\n")
--- meshtp.py
+++ meshtp.py
@@ -1,122 +1,100 @@
 #!/usr/bin/python
-import meshtastic
-import meshtastic.serial_interface
-import time
+import time, os, meshtastic.serial_interface, meshtastic, math
 from pubsub import pub
 import sys
 import hashlib
-import mesh
 
-def printHelpCommand():
+def printHelpCommand(): # print help
     messigefile = open("help.txt", "r")
     print(messigefile.read())
     messigefile.close()
-
-if len(sys.argv) < 4:
+print(len(sys.argv)) # print number of args
+if len(sys.argv) < 4: # ensure correft number of args
     print("incorrect number of argument\n")
     printHelpCommand()
     sys.exit(1)
 
-for i in sys.argv:
+for i in sys.argv: # check for --help
     if i == "--help":
         printHelpCommand() # explain the command line options
         sys.exit(0)
 
-isServer = False
-sendOrRecive = sys.argv[1]
-if sendOrRecive == "send":
+isServer = False # set default server val
+sendOrRecive = sys.argv[1] # send or recive is set to arg 1
+if sendOrRecive == "send": # set isServer based on sendOrRecive
     isServer = False;
-elif sendOrRecive == "recive":
+elif sendOrRecive == "recive": # set isServer based on sendOrRecive
     isServer = True;
-else:
+else: # inclorrect syntax and print help
     print("incorrect program role\n")
     printHelpCommand() # explain the command line options
     sys.exit(1)
 
-filename = sys.argv[2]
+if isServer == False:
 
-destinationID = sys.argv[3]
+    filename = sys.argv[2] # name of file
+    destination_ID='!433ce1d4' # hex id of destination node
+    numberOfPakets = math.ceil(os.path.getsize(filename) / 198)
 
-masterPacket = ""
-hexpackets = []
-packets = []
-file = open(filename, "rb")
+    file = open(filename, "rb") # open the file
+    masterHash = hashlib.file_digest(file, "sha256").hexdigest()[:16] #crate the hash of the original file
 
-hexString = ''.join([f'{byte:02x}' for byte in file.read()])
-file.seek(0)
+    if numberOfPakets > 16777215: # make sure the number of packets can be represented with 6 bytes of hex
+        file.close() # close the new file
+        print("file is to large")
+        printHelpCommand()
+        sys.exit(1)
+    file.seek(0)
+    for i in range(3):
+        hexbytes = file.read(198)
+        packet = ''.join((f"{i:06x}",f"{len(hexbytes):02x}",hashlib.sha256(hexbytes).hexdigest()[:4], hexbytes.decode("utf-8")))
 
-masterHash = hashlib.file_digest(file, "sha256").hexdigest()[:16]
-#print ("\n\nmasterHash: " + masterHash)
-
-
-hexpackets = [hexString[i:i+190] for i in range(0, len(hexString), 190)]
-#print(hexpackets)
-
-if len(hexpackets) >= 65530:
-    print("file is to large")
-    sys.exit(1)
-
-n = 1
-
-for i in hexpackets:
-    string = f"{n:04x}" + f"{len(i):02x}" + hashlib.sha256(i.encode("utf8")).hexdigest()[:4] + i
-    n+=1
-    packets.append(string)
-
-newhex = ''
-
-for i in packets:
-    datalen = int(i[4:6], 16)
-    newhex = newhex + i[10:200]
-print (newhex==hexString)
-#hashlib.sha256(file.read()).hexdigest()[:16]
-file2 = open("/home/lucas/Documents/MeshTP/thing.txt", "wb")
-file2.write(bytes.fromhex(newhex))
-
-file2.close()
-
+    file.close() # close the new file
 
 
 '''
-file2 = open("/home/lucas/Documents/MeshTP/thing.png", "wb")
-file2.write(bytes.fromhex(hexString))
 
-file.close()
-file2.close()
 
-file3 = open(filename, "rb")
 
-masterHash2 = hashlib.file_digest(file3, "sha256").hexdigest()[:16]
-print (masterHash2)
+for i in range(len(packets)): # generate the packets with the header and the payload
+    
 
-hexString = [hexString[i:i+192] for i in range(0, len(hexString), 192)]
+newhex = '' # create newhex variable
 
-hashes = []
+for i in packets: # reasemble the hex data to create the original hexString variable
+    datalen = int(i[6:8], 16)
+    newhex = newhex + i[12:210]
+print (newhex==hexString) # check to make sure it encoded and decoded it correctly (the hexString and the newhex strings are the same)
+#hashlib.sha256(file.read()).hexdigest()[:16]
+file2 = open("/home/lucas/Documents/MeshTP/thing.txt", "wb") # open a new file
+file2.write(bytes.fromhex(newhex)) # write the decoded data to a new file
 
-for i in hexString:
-    hashes.append(hashlib.sha256(i.encode('utf-8')).hexdigest()[:4])
-print(hashes)
-
+file2.close() # close the new file
 
 
 
 
-
-#destinationId='!433ce1d4'
 
 def onReceive(packet, interface):
+    #print(packet)
+    #print("\n\n\n")
+    message = packet.get("decoded", {}).get("payload", b"").decode("utf-8", errors="ignore")
+    #print(f"From: {packet.get("from")} in channel {packet.get("channel")} - {message}")
+    #print("\n\n\n")
+    #print("\n\n\n\n\n\n\n\n\n")
+    
     if "decoded" not in packet:
         return
-    if packet["decoded"]["portnum"] == "TEXT_MESSAGE_APP":
-        print(packet)
-        message = packet.get("decoded", {}).get("payload", b"").decode("utf-8", errors="ignore")
-        print(f"From: {packet.get("from")} in channel {packet.get("channel")} - {message}")
+    # if packet["decoded"]["portnum"] == "TEXT_MESSAGE_APP":
+    
+    message = packet.get("decoded", {}).get("payload", b"").decode("utf-8", errors="ignore")
+    print(f"From: {packet.get("from")} in channel {packet.get("channel")}")
+    return
 
 def onConnection(interface, topic=pub.AUTO_TOPIC): # called when we (re)connect to the radio
-    print("device connected")
-    interface.sendText("hello mesh", channelIndex=1)
+    pass
 
-interface = meshtastic.serial_interface.SerialInterface(devPath='/dev/ttyACM0')
+interface = meshtastic.serial_interface.SerialInterface(devPath='/dev/ttyUSB0')
 
 pub.subscribe(onReceive, "meshtastic.receive")
 pub.subscribe(onConnection, "meshtastic.connection.established")
@@ -126,4 +104,5 @@ try:
         time.sleep(1000)
 except KeyboardInterrupt:
     print("Disconnecting...")
-    interface.close()''' 
+    interface.close()
+    '''
